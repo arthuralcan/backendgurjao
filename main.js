e2660a120d625a4bc8b4c07a45782ded5f3ca1c9
Arthur Alcantara de Andrade <142822030+arthuralcan@users.noreply.github.com>
2023-11-09 10:49:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,26 +4,26 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// Criando servidor web com express
-const app = express();
-const router = express.Router();
 
-// Configurando servidor web
+// criando servidor web com express
+const app = express();
+
+// configurando seridor web
 console.log("Configurando servidor...");
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// Conectando os controllers ao router
-router.use("/noticias", require("./controllers/noticias_controller"));
-router.use("/secretarias", require("./controllers/secretarias_controller"));
-
-app.use("/", router);
+// conectando os controllers ao servidor web
+app.use("/noticias", require("./controllers/noticias_controller"));
+app.use("/secretarias", require("./controllers/secretarias_controller"));
 
 console.log("Conectando banco de dados...");
 mongoose.connect(process.env.URL_BANCO).then(() => {
     console.log("Iniciando banco de dados...");
     app.listen(parseInt(process.env.PORTA_SERVIDOR), () => {
-        console.log(`O servidor está conectado e funcionando.`);
+        console.log(`o servidor conectado, funcionado.`);
     });
 });
+ 
+
